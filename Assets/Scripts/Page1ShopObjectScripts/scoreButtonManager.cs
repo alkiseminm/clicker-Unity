59a13391152255505931a9e60ec083e06a6cf372
alkiseminm <alkiseminmuhammed@gmail.com>
2025-03-23 01:06:35 +0300

    Update 2.13
    - Tidying up the place.

--- Assets/Scripts/Page1ShopObjectScripts/scoreButtonManager.cs
+++ Assets/Scripts/Page1ShopObjectScripts/scoreButtonManager.cs
@@ -1,16 +1,52 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class scoreButtonManager : MonoBehaviour
 {
+    public scoreManager scoreManager;
+    
+    public TextMeshProUGUI ButtonText;
+    public TextMeshProUGUI CalculateText;
+
+    public int add;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CalculateAdd();
+        UpdateButtonText();
+        UpdateCalculatorText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateAdd();
+        UpdateButtonText();
+        UpdateCalculatorText();
+    }
+
+    void CalculateAdd()
+    {
+        add = scoreManager.plus * scoreManager.mult;
+    }
+
+    void UpdateButtonText()
+    {
+        if (ButtonText != null)
+        {
+            ButtonText.text = "+" + add.ToString();
+        }
+    }
+
+    void UpdateCalculatorText()
+    {
+        CalculateText.text = "Plus: +" + scoreManager.plus.ToString() + "\n" + "Multiplier : x" + scoreManager.mult.ToString();
+    }
+
+    public void ScoreButtonPressed()
+    {
+        scoreManager.score += add;
     }
 }
--- Assets/Scripts/scoreManager.cs
+++ Assets/Scripts/scoreManager.cs
@@ -5,56 +5,24 @@ using Unity.VisualScripting;
 public class scoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI ButtonText;
-    public TextMeshProUGUI CalculateText;
 
     public int plus = 1;
     public int mult = 1;
-    public int add;
 
     public int score = 0;
     
     void Start()
     {
-        CalculateAdd();
         UpdateScoreText();
-        UpdateButtonText();
-        UpdateCalculatorText();
     }
 
     private void Update()
     {
-        CalculateAdd();
         UpdateScoreText();
-        UpdateButtonText();
-        UpdateCalculatorText();
-    }
-
-    void CalculateAdd()
-    {
-        add = plus * mult;
-    }
-    
-    void UpdateCalculatorText()
-    {
-        CalculateText.text = "Plus: +" + plus.ToString() + "\n" + "Multiplier : x" + mult.ToString();
-    }
-    
-    void UpdateButtonText()
-    {
-        if (ButtonText != null)
-        {
-            ButtonText.text = "+" + add.ToString();
-        }
     }
 
     void UpdateScoreText()
     {
         scoreText.text = score.ToString();
-    }
-
-    public void IncreaseScore()
-    {
-        score += add;
     }
 }
